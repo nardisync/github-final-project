88aaaf448107ea432913eaa1cef7d358afdc45a4
Marco <78350570+nardisync@users.noreply.github.com>
2022-06-24 14:55:39 +0200

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,9 +4,9 @@
 
 # Do not use this in production. Sample purpose only.
 
-# Author: Upkar Lidder (IBM)
+# Author: Marco Nardi (IBM)
 # Additional Authors:
-# <your GitHub username>
+# nardisync
 
 # Input:
 # p, principal amount
